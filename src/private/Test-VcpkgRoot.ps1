54a6980489c59beab264f3e76717375884cb7c2e
Demian M. Nave <dnave@navehaus.com>
2025-08-14 14:43:50 -0400

    Use Join-RelativePath for extra checking

--- src/private/Test-VcpkgRoot.ps1
+++ src/private/Test-VcpkgRoot.ps1
@@ -1,18 +1,20 @@
 Set-StrictMode -Version 3.0
 
+. $PSScriptRoot\Join-RelativePath.ps1
+
 function Test-VcpkgRoot {
     param(
         [Parameter(Mandatory = $true, Position = 0)]
         [string]$Path
     )
 
-    $private:dir = Resolve-Path -Path $Path -Force -ErrorAction Ignore
-    if($null -eq $dir) {
+    $private:dir = Join-RelativePath -Path $Path -ChildPath . -Resolve -ErrorAction Ignore
+    if ($null -eq $dir) {
         return $false
     }
 
     try {
-        Join-Path -Path $dir -ChildPath '.vcpkg-root' -Resolve -ErrorAction Stop | Out-Null
+        Join-RelativePath -Path $dir -ChildPath '.vcpkg-root' -Resolve -ErrorAction Stop | Out-Null
         return $true
     }
     catch {
