4ee935bc91503c4f8681d6e8a5686f323ca3222d
Demian M. Nave <dnave@navehaus.com>
2026-02-12 10:16:11 -0500

    feat: support multipart normalized paths

--- src/private/ConvertTo-NormalizedPath.ps1
+++ src/private/ConvertTo-NormalizedPath.ps1
@@ -6,12 +6,20 @@ function ConvertTo-NormalizedPath {
     <#
     .SYNOPSIS
     Removes '.', '..', and '\.' from Path, and if Path is relative, returns an absolute path based on the current working directory. This function works even if Path does not exist.
+
+    .PARAMETER Path
+    The absolute or relative path to normalize. An error is raised if Path contains non-path characters.
+
+    .PARAMETER ChildPath
+    An optional relative path to append to Path. An error is raised if ChildPath contains non-path characters, or if it is an absolute path.
     #>
 
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
-        [string]$Path
+        [string]$Path,
+        [Parameter(Mandatory = $false, Position = 1)]
+        [string]$ChildPath = '.'
     )
 
     begin {
@@ -21,7 +29,7 @@ function ConvertTo-NormalizedPath {
     }
 
     process {
-        # [System.IO.Path]::GetFullPath((Join-Path))
-        return $PSCmdLet.GetUnresolvedProviderPathFromPSPath((Join-RelativePath -Path $Path -ChildPath .))
+        $paths = @{ 'Path' = $Path; 'ChildPath' = $ChildPath }
+        return $PSCmdLet.GetUnresolvedProviderPathFromPSPath((Join-RelativePath @paths))
     }
 }
